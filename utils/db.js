2941abb72fe794843afee1a0e30a05fb2e452a77
EmmanuelDarko <edarko322@gmail.com>
2023-08-11 23:52:50 +0000

    added

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const mongo = () => {
   return mongoose
-    .connect(
-      { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-      process.env.DB_URL
-    )
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
     .then(() => console.log("connection successfull"))
     .catch((err) => err.message);
 };
