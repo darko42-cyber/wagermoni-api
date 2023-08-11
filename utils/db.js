92c88080b8c1465a64f9fbd58beba55786288b0c
EmmanuelDarko <edarko322@gmail.com>
2023-08-11 23:40:21 +0000

    changes

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  process.env.DB_URL
+);
 
 const connection = mongoose.connection;
 
