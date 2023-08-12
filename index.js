a73286f3cb67a5d826d70a47b9cc8cfa7668a9ec
EmmanuelDarko <edarko322@gmail.com>
2023-08-12 00:27:23 +0000

    changed

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const commentsRoute = require("./routes/comments");
 const likesRoute = require("./routes/likes");
 const oddsEventRoute = require("./routes/oddsEvent");
 const dbConfig = require("./utils/db");
-dbConfig();
 
 // app.use("/api/users", usersRoute);
 app.use("/api/predictions", predictionsRoute);
@@ -25,5 +24,6 @@ app.use("/api/likes", likesRoute);
 app.use("/api/events", oddsEventRoute);
 
 app.listen(process.env.PORT, (res) => {
+  dbConfig();
   console.log("Server is listening on port 5000");
 });
